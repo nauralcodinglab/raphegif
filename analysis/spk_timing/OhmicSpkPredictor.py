f97134c341c6fe2789b039cd50bcb6c52366a30d
Emerson Harkin <emerson.f.harkin@gmail.com>
2018-12-03 14:54:52 -0500

    GABA latency figure. Modifications to Cell tau fitting procedure and OhmicSpikePredictor.

---
+++ analysis/spk_timing/OhmicSpkPredictor.py
@@ -0,0 +1,239 @@
+#%% IMPORT MODULES
+
+from __future__ import division
+
+from copy import deepcopy
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm
+
+import sys
+sys.path.append('./analysis/gating/')
+from cell_class import Cell, Recording
+
+
+#%% DEFINE SPK TIME PREDICTOR
+
+class OhmicSpkPredictor(object):
+
+    def __init__(self):
+        pass
+
+    def add_recordings(self, recs, baseline = None, ss = None, V_channel = 0, I_channel = 1, dt = 0.1):
+
+        """
+        Recs should be a list of Recording objects with dimensionality [channel, time, sweep]
+        """
+
+        recs_arr = np.array(recs)
+        self.V = deepcopy(recs_arr[:, V_channel, :, :])
+        self.I = deepcopy(recs_arr[:, I_channel, :, :])
+
+        self.dt = dt
+
+        if type(recs[0]) is Recording and (baseline is not None and ss is not None):
+
+            self.Rins = []
+
+            for rec in recs:
+                tmp = rec.fit_test_pulse(baseline, ss, V_clamp = False, verbose = False)
+                self.Rins.append(tmp['R_input'].mean())
+
+    @property
+    def t_vec(self):
+
+        return np.arange(0, (self.V.shape[1] - 0.5) * self.dt, self.dt)
+
+    @property
+    def t_mat(self, reps = 'default'):
+
+        if reps == 'default':
+            reps = self.V.shape[2]
+
+        return np.tile(self.t_vec[:, np.newaxis], (1, reps))
+
+
+    def phase_plot(self):
+
+        dV = np.gradient(self.V, axis = 1)
+
+        plt.figure()
+        for i in range(self.V.shape[0]):
+            plt.plot(
+                self.V[i, :, :], dV[i, :, :] / self.dt,
+                color = cm.coolwarm(i / self.V.shape[0]),
+                alpha = 1/self.V.shape[0]
+            )
+
+        plt.xlabel('V (mV)')
+        plt.ylabel('dV/dt (mV/ms)')
+
+        plt.show()
+
+
+    def scrape_data(self, V0_range = (2495, 2595), Vinf_range = (2750, 2800), baseline_range = (0, 100),
+        quiescent_until = 2600, dVdt_thresh = 10):
+
+        """
+        Scrape spks, V0, and Vinf from a set of recordings.
+
+        Inputs:
+
+            V0_range: tuple of two floats
+            --  Time (ms) range from which to get V0
+
+            Vinf_range: tuple of two floats
+            --  Time (ms) range from which to calculate Vinf *based on I_probe*
+
+            quiescent_until: float
+            --  Discard sweeps that have spikes before this point (ms).
+
+            dVdt_thresh: float
+            --  Voltage derivative threshold to use for spk detection (mV/ms).
+
+        Places all spk times together in a big honking list. Complementary big honking lists of V0 and Vinf are generated.
+        Only takes the first spk after `quiescent_until` from each sweep.
+        """
+
+        V0_slice        = slice(int(V0_range[0] / self.dt), int(V0_range[1] / self.dt))
+        Vinf_slice      = slice(int(Vinf_range[0] / self.dt), int(Vinf_range[1] / self.dt))
+        baseline_slice  = slice(int(baseline_range[0] / self.dt), int(baseline_range[1] / self.dt))
+
+        dVdt = np.gradient(self.V, axis = 1) / self.dt
+
+        spks = []
+        V0 = []
+        Vinf = []
+
+        for rec_ind in range(dVdt.shape[0]):
+
+            V_baseline = self.V[:, baseline_slice, :].mean()
+            I_baseline = self.I[:, baseline_slice, :].mean()
+
+            for sw_ind in range(dVdt.shape[2]):
+
+                # Get spk inds for this recording.
+                spks_i = np.where(dVdt[rec_ind, :, sw_ind] > dVdt_thresh)[0] * self.dt
+
+                # Skip sweep if premature spks are detected.
+                if any(spks_i < quiescent_until):
+                    continue
+
+                # Skip sweep if no spks found.
+                if len(spks_i) < 1:
+                    continue
+
+                # Extract V0
+                V0_i = self.V[rec_ind, V0_slice, sw_ind].mean()
+
+                # Extract Vinf
+                I_probe = self.I[rec_ind, Vinf_slice, sw_ind].mean()
+                Vinf_i = self.Rins[rec_ind] * (I_probe - I_baseline) * 1e-3 + V_baseline
+
+                # Assign output
+                spks.append(spks_i[0] - quiescent_until)
+                V0.append(V0_i)
+                Vinf.append(Vinf_i)
+
+        # Store in class attributes
+        self.spks   = np.array(spks)
+        self.V0     = np.array(V0)
+        self.Vinf   = np.array(Vinf)
+
+
+    def plot(self):
+
+        plt.figure()
+
+        spec = plt.GridSpec(2, 1, height_ratios = [2, 1])
+
+        plt.subplot(spec[0, :])
+        for i in range(self.V.shape[0]):
+            plt.plot(
+                self.t_mat,
+                self.V[i, :, :],
+                color = cm.coolwarm(i / self.V.shape[0]),
+                alpha = 1/self.V.shape[0]
+            )
+        plt.plot(self.spks, [-30 for i in self.spks], 'bx')
+
+        plt.subplot(spec[1, :])
+        for i in range(self.I.shape[0]):
+            plt.plot(
+                self.t_mat,
+                self.I[i, :, :],
+                color = cm.coolwarm(i / self.V.shape[0]),
+                alpha = 1/self.V.shape[0]
+            )
+
+
+    def fit_spks(self, thresh_guesses = 'default', Vinf_guesses = 'default', force_tau = None, verbose = False):
+
+        if thresh_guesses == 'default':
+            thresh_guesses = np.linspace(-50, 0, 250)
+
+        if Vinf_guesses == 'default':
+            margin = self.Vinf.mean() * 2.
+            Vinf_guesses = np.linspace(
+                self.Vinf.mean() - margin,
+                self.Vinf.mean() + margin,
+                250
+            )
+
+        tau_est = []
+        Vinf_ = []
+        thresh_ = []
+        SSE = []
+
+        y = np.array(self.spks)
+
+        for i, thresh in enumerate(thresh_guesses):
+            for j, Vinf in enumerate(Vinf_guesses):
+
+                if verbose:
+                    print '\rFitting {:.1f}%'.format(100 * (i+1)/len(thresh_guesses)),
+
+                X = - np.log( (thresh - Vinf) / (self.V0 - Vinf) )
+                if force_tau is None:
+                    XTX = np.dot(X.T, X)
+                    XTX_inv = 1/XTX
+                    XTY = np.dot(X.T, y)
+
+                    b = np.dot(XTX_inv, XTY)
+                else:
+                    b = force_tau
+
+                tau_est.append(b)
+
+                yhat = np.dot(X, b)
+                SSE.append(np.sum( (y - yhat)**2 ))
+
+                Vinf_.append(Vinf)
+                thresh_.append(thresh)
+
+        self.tau = tau_est[np.nanargmin(SSE)]
+        self.thresh = thresh_[np.nanargmin(SSE)]
+        self.Vinf_est = Vinf_[np.nanargmin(SSE)]
+
+        return tau_est, SSE
+
+
+    def predict_spks(self, **kwargs):
+
+        """
+        Valid kwargs are: tau, thresh, V0, Vinf
+        """
+
+        valid_kwargs = ['tau', 'thresh', 'V0', 'Vinf']
+        if any([key not in valid_kwargs for key in kwargs.keys()]):
+            raise NameError('Valid kwargs are: {}'.format(', '.join(valid_kwargs)))
+
+        tau         = kwargs.get('tau', self.tau)
+        thresh      = kwargs.get('thresh', self.thresh)
+        V0          = kwargs.get('V0', self.V0)
+        Vinf        = kwargs.get('Vinf', self.Vinf)
+
+        spk_prediction = - tau * np.log( (thresh - Vinf) / (V0 - Vinf) )
+
+        return spk_prediction
---
+++ figs/scripts/gaba_neurons/spk_latency_phenotypes.py
@@ -0,0 +1,237 @@
+#%% IMPORT MODULES
+
+from __future__ import division
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gs
+import pandas as pd
+
+import sys
+sys.path.append('./analysis/spk_timing/')
+sys.path.append('./analysis/gating')
+sys.path.append('./figs/scripts')
+import OhmicSpkPredictor as osp
+from cell_class import Cell
+import pltools
+
+
+#%% LOAD DATA
+
+DATA_PATH = './data/GABA_cells/spk_time/'
+
+inventory = pd.read_csv(DATA_PATH + 'inventory.csv')
+inventory['cumcount'] = inventory.groupby('Cell_ID').cumcount()
+fnames = inventory.pivot('Cell_ID', 'cumcount', values = 'Recording').values.tolist()
+fnames = [[f for f in fs if f is not None] for fs in fnames] # Remove `None`s
+
+cellnames = inventory.pivot('Cell_ID', 'cumcount', values = 'Recording').index.tolist()
+
+# Manually reject recordings.
+rejects = ['18711002.abf']
+
+taus = []
+cells = []
+for i in range(len(fnames)):
+    ce_tmp = Cell().read_ABF([DATA_PATH + fname for fname in fnames[i] if fname not in rejects])
+    taus_tmp = []
+    for rec in ce_tmp:
+        rec.set_dt(0.1)
+        out = rec.fit_test_pulse(
+            (0, 100), (5000, 5100), V_clamp = False, tau = (1650, 5000),
+            V_chan = 0, I_chan = 1,
+            verbose = False, plot_tau = True)
+        taus_tmp.append(out['tau'])
+    print('Tau: {:.1f} +- {:.1f}ms'.format(np.mean(taus_tmp), np.std(taus_tmp)))
+    taus.append(np.mean(taus_tmp))
+    cells.append(ce_tmp)
+
+
+
+#%%
+
+cells[0][0].plot()
+cells[0][0].set_dt(0.1)
+cells[0][0].fit_test_pulse((0, 100), (5000, 5100), V_clamp = False, tau = (1620, 1800, 3000))
+
+#%%
+test_cell = cells[0]
+test_cell[0].shape
+pred = osp.OhmicSpkPredictor()
+pred.add_recordings(test_cell, (0, 100), (5000, 5100))
+pred.scrape_data(quiescent_until = 2662)
+pred.spks
+pred.V0
+
+plt.figure()
+plt.plot(pred.V0, pred.spks, 'ko')
+
+#%% SCRAPE SPIKE LATENCIES
+
+predictors = []
+
+for tau, ce in zip(taus, cells):
+    pred_tmp = osp.OhmicSpkPredictor()
+    pred_tmp.add_recordings(ce, (0, 100), (5000, 5100))
+    pred_tmp.scrape_data(quiescent_until = 2662)
+    pred_tmp.fit_spks(force_tau = tau, Vinf_guesses = [pred_tmp.Vinf.mean()], thresh_guesses = np.linspace(-100, 200, 600))
+
+    predictors.append(pred_tmp)
+
+
+
+#%% INSPECT SCRAPED SPIKE LATENCIES
+plt.style.use('./figs/scripts/thesis/thesis_mplrc.dms')
+
+tr_xlim = slice(16000, 33000)
+
+#spec_outer = gs.GridSpec(len(cells), )
+
+
+
+for id, ce, pr in zip(cellnames, cells, predictors):
+
+    plt.figure(figsize = (6, 3))
+
+    plt.subplot(121)
+    plt.title('{}'.format(id))
+    plt.plot(ce[0][0, tr_xlim, ::4], 'k-', lw = 0.5)
+    plt.xticks([])
+
+    plt.subplot(122)
+    plt.plot(pr.V0, pr.spks, 'ko', label = 'Observed spikes')
+    V0s = np.linspace(-110, -40, 200)
+    plt.plot(
+        V0s, pr.predict_spks(V0 = V0s, Vinf = pr.Vinf_est),
+        'r--', label = 'Ohmic model\n$\\hat{{\\theta}} = $ {:.1f}mV'.format(pr.thresh)
+    )
+    plt.plot(
+        V0s, pr.predict_spks(V0 = V0s, Vinf = pr.Vinf_est, thresh = -45),
+        'g--', label = 'Ohmic model\n$\\theta$ fixed at $-45$mV'
+    )
+    plt.axvline(pr.Vinf_est, color = 'k', lw = 0.5)
+    """plt.text(
+        0.9, 0.9, r'$\hat{{\theta}}$ = {:.1f}mV'.format(pr.thresh),
+        ha = 'right', va = 'top', transform = plt.gca().transAxes)"""
+    plt.ylim(0, plt.ylim()[1])
+    plt.xlim(-119, -37)
+    plt.ylabel('Latency (ms)')
+    plt.xlabel('$V_0$ (mV)')
+    plt.legend()
+
+    plt.tight_layout()
+    plt.show()
+
+#%%
+
+IMG_PATH = './figs/ims/gaba_cells/'
+
+plt.style.use('./figs/scripts/thesis/thesis_mplrc.dms')
+
+tr_xrange = slice(15000, 30000)
+
+tr_params = {
+    'sweeps': [3, -2, -1],
+    'cols': ['k', 'k', 'k'],
+    'alphas': [1, 0.5, 0.3]
+}
+
+def plot_traces(rec, primary_ax, secondary_ax, param_dict, primary_channel = 0, secondary_channel = 1, dt = 0.1):
+
+    sweeps  = param_dict['sweeps']
+    cols    = param_dict['cols']
+    alphas  = param_dict['alphas']
+
+    if not all([len(sweeps) == len(x) for x in [cols, alphas]]):
+        raise ValueError('sweeps, cols, and alphas not of identical lengths.')
+
+    t_vec = np.arange(0, (rec.shape[1] - 0.5) * dt, dt)
+
+    if primary_ax is not None:
+        for sw, col, alph in zip(sweeps, cols, alphas):
+            primary_ax.plot(
+                t_vec, rec[primary_channel, :, sw],
+                '-', color = col, lw = 0.5, alpha = alph
+            )
+    if secondary_ax is not None:
+        for sw, col, alph in zip(sweeps, cols, alphas):
+            secondary_ax.plot(
+                t_vec, rec[secondary_channel, :, sw],
+                '-', color = 'gray', lw = 0.5, alpha = alph
+            )
+
+spec_outer = gs.GridSpec(len(cells), 2)
+
+plt.figure(figsize = (6, 8))
+
+Letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+           'O', 'P', 'Q', 'R', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+for i in range(len(cells)):
+
+    cename   = cellnames[i]
+    ce      = cells[i]
+    pr      = predictors[i]
+
+    spec_I_tmp = gs.GridSpecFromSubplotSpec(2, 1, spec_outer[i, 0], height_ratios = [1, 0.2], hspace = 0)
+
+    I_Vax_tmp = plt.subplot(spec_I_tmp[0, :])
+    I_Vax_tmp.set_title(r'\textbf{{{}1}} {} current steps'.format(Letters[i], cename), loc = 'left')
+    I_Vax_tmp.set_xticks([])
+    I_Vax_tmp.set_ylabel('$V$ (mV)')
+    I_Vax_tmp.set_ylim(-105, 10)
+    I_Vax_tmp.set_yticks([-100, -50, 0])
+    pltools.hide_border('trb', I_Vax_tmp)
+    I_Iax_tmp = plt.subplot(spec_I_tmp[1, :])
+    I_Iax_tmp.set_yticks([])
+    pltools.hide_border('ltr', I_Iax_tmp)
+    if i == (len(cells) - 1):
+        I_Iax_tmp.set_xlabel('Time (ms)')
+    else:
+        I_Iax_tmp.set_xticks([])
+        pltools.hide_border('b', I_Iax_tmp)
+
+    plot_traces(ce[0][:, tr_xrange, :], I_Vax_tmp, I_Iax_tmp, tr_params)
+    I_Vax_tmp.axhline(pr.thresh, color = 'r', lw = 0.5, ls = '--', dashes = (5, 5))
+    if i == 0:
+        I_Vax_tmp.annotate(
+            r'$\hat{{\theta}}$',
+            (120, pr.thresh), xytext = (20, 0), ha = 'center', va = 'top',
+            arrowprops = {'arrowstyle':'->'}
+        )
+
+    plt.subplot(spec_outer[i, 1])
+    plt.title(r'\textbf{{{}2}} {} spike latencies'.format(Letters[i], cename), loc = 'left')
+    plt.plot(pr.V0, pr.spks, 'ko')
+    V0s = np.linspace(-110, -40, 200)
+    plt.plot(
+        V0s, pr.predict_spks(V0 = V0s, Vinf = pr.Vinf_est),
+        'r-', label = 'Ohmic model\n$\\hat{{\\theta}} = {:.1f}$mV'.format(pr.thresh)
+    )
+    if i == 0:
+        plt.annotate(
+            r'$\hat{{\theta}}$',
+            (pr.thresh, 0), (-70, 20),
+            ha = 'center', va = 'bottom',
+            arrowprops = {'arrowstyle': '->'}
+        )
+        plt.annotate(
+            r'$V_\infty$',
+            (pr.Vinf_est, 130), (-60, 175),
+            ha = 'center', va = 'center',
+            arrowprops = {'arrowstyle': '->'}
+        )
+    plt.axvline(pr.Vinf_est, color = 'k', lw = 0.5)
+    plt.ylim(0, plt.ylim()[1])
+    plt.xlim(-119, -37)
+    plt.ylabel('Latency (ms)')
+    if i == (len(cells) - 1):
+        plt.xlabel('$V_0$ (mV)')
+    plt.legend()
+    pltools.hide_border('tr')
+
+plt.tight_layout()
+
+if IMG_PATH is not None:
+    plt.savefig(IMG_PATH + 'steps_latencies.png')
+
+plt.show()
