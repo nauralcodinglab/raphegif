825dc217038b6d872e71382066b0398350f010d4
Emerson Harkin <emerson.f.harkin@gmail.com>
2018-12-04 13:53:43 -0500

    Inspect vclamp SOM nickel washon recordings.

---
+++ figs/scripts/gaba_neurons/nickel_wc.py
@@ -0,0 +1,40 @@
+#%% IMPORT MODULES
+
+from __future__ import division
+
+import os
+import shutil
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gs
+import pandas as pd
+
+import sys
+sys.path.append('./analysis/spk_timing/')
+sys.path.append('./analysis/gating')
+sys.path.append('./figs/scripts')
+import OhmicSpkPredictor as osp
+from cell_class import Cell
+import pltools
+
+
+#%% LOAD DATA
+
+DATA_PATH = './data/GABA_cells/nickel/'
+
+fnames = {
+    'bl_steps': ['18n16006.abf', '18n16007.abf', '18n16008.abf'],
+    'ni_washin': ['18n16009.abf'],
+    'ni_steps': ['18n16010.abf'],
+    'wash': ['18n16011.abf']
+}
+recs = {}
+
+for key in fnames.keys():
+    recs[key] = Cell().read_ABF([DATA_PATH + f for f in fnames[key]])
+
+#%%
+
+recs['bl_steps'][0].plot()
+recs['ni_washin'][0].plot()
