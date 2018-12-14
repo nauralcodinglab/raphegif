7d4d1bf3f87f9fd8179c47902baf6a33ce03c216
Emerson Harkin <emerson.f.harkin@gmail.com>
2018-12-14 16:36:36 -0500

    Analyze intrinsic reliability of 5HT, SOM, and pyr neurons

---
+++ analysis/exclusion/md_window_size.py
@@ -0,0 +1,178 @@
+#%% IMPORT MODULES
+
+from __future__ import division
+
+import os
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gs
+import seaborn as sns
+import pandas as pd
+from scipy import stats
+
+import sys
+sys.path.append('./src')
+sys.path.append('./figs/scripts')
+
+from Experiment import *
+from SpikeTrainComparator import intrinsic_reliability
+
+import pltools
+
+
+#%% DEFINE FUNCTION TO GAG VERBOSE POZZORINI FUNCTIONS
+
+class gagProcess(object):
+
+    def __enter__(self):
+        self._original_stdout = sys.stdout
+        sys.stdout = open(os.devnull, 'w')
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        sys.stdout = self._original_stdout
+
+
+#%% LOAD DATA
+
+data_paths = {
+    '5HT': './data/fast_noise_5HT/',
+    'GABA': './data/GABA_cells/',
+    'mPFC': './data/mPFC/mPFC_spiking/'
+}
+
+fname_paths = {
+    '5HT': './data/fast_noise_5HT/index.csv',
+    'GABA': './data/GABA_cells/index.csv',
+    'mPFC': './data/mPFC/fnames.csv'
+}
+
+experiments = {
+    '5HT': [],
+    'GABA': [],
+    'mPFC': []
+}
+
+# Read in GABA and 5HT cells
+for key in ['5HT', 'GABA']:
+
+    file_index = pd.read_csv(fname_paths[key])
+
+    for i in range(file_index.shape[0]):
+        with gagProcess():
+
+            tmp_experiment = Experiment(file_index.loc[i, 'Cell'], 0.1)
+            tmp_experiment.setAECTrace(
+                'Axon', fname = data_paths[key] + file_index.loc[i, 'AEC2'],
+                V_channel = 0, I_channel = 1
+            )
+
+            for ind in ['1', '2', '3']:
+
+                tmp_experiment.addTrainingSetTrace(
+                    'Axon', fname = data_paths[key] + file_index.loc[i, 'Train' + ind],
+                    V_channel = 0, I_channel = 1
+                )
+                tmp_experiment.addTestSetTrace(
+                    'Axon', fname = data_paths[key] + file_index.loc[i, 'Test' + ind],
+                    V_channel = 0, I_channel = 1
+                )
+
+            for tr in tmp_experiment.testset_traces:
+                tr.detectSpikes()
+
+
+        experiments[key].append(tmp_experiment)
+
+# Exclude GABA0 because of problems.
+experiments['GABA'].pop(0)
+
+
+# Read in PFC files, which are formatted slightly differently.
+fnames = pd.read_csv(fname_paths['mPFC'])
+
+for i in range(fnames.shape[0]):
+
+    if fnames.loc[i, 'TTX'] == 0:
+
+        with gagProcess():
+
+            tmp_experiment = Experiment(fnames.loc[i, 'Experimenter'] + fnames.loc[i, 'Cell_ID'], 0.1)
+            tmp_experiment.setAECTrace(FILETYPE = 'Axon', fname = data_paths['mPFC'] + fnames.loc[i, 'AEC'],
+                V_channel = 0, I_channel = 1)
+            tmp_experiment.addTrainingSetTrace(FILETYPE = 'Axon', fname = data_paths['mPFC'] + fnames.loc[i, 'Train'],
+                V_channel = 0, I_channel = 1)
+            tmp_experiment.addTestSetTrace(FILETYPE = 'Axon', fname = data_paths['mPFC'] + fnames.loc[i, 'Test'],
+                V_channel = 0, I_channel = 1)
+
+            for tr in tmp_experiment.testset_traces:
+                tr.detectSpikes()
+
+        experiments['mPFC'].append(tmp_experiment)
+
+    else:
+        continue
+
+#%% EXTRACT RELIABILITIES
+windows = np.logspace(0, 2, 30)
+
+reliabilities = {
+    '5HT' : [],
+    'GABA': [],
+    'mPFC': []
+}
+windows_arrs = {}
+
+for key in experiments.keys():
+    print('{}'.format(key))
+    for i, win in enumerate(windows):
+        print('Extracting reliability {:.1f}%'.format(100*(i + 1)/len(windows)))
+        reliabilities_tmp = []
+        for expt in experiments[key]:
+            reliabilities_tmp.append(intrinsic_reliability(expt.testset_traces, win, 0.1))
+
+        reliabilities[key].append(reliabilities_tmp)
+
+    reliabilities[key] = np.array(reliabilities[key])
+    windows_arrs[key] = np.tile(windows[:, np.newaxis], (1, reliabilities[key].shape[1]))
+
+#%%
+
+experiments['GABA'][0].testset_traces
+
+#%% CREATE FIGURE
+
+IMG_PATH = './figs/ims/exclusion/'
+
+plt.style.use('./figs/scripts/thesis/thesis_mplrc.dms')
+
+plt.figure(figsize = (6, 3))
+
+plt.subplot(131)
+plt.title(r'\textbf{A} mPFC L5 pyramidal', loc = 'left')
+plt.axvline(8, color = 'k', ls = '--', dashes = (10, 10), lw = 0.5)
+plt.semilogx(windows_arrs['mPFC'], reliabilities['mPFC'], 'k-', lw = 0.8, alpha = 0.8)
+plt.ylim(0,1)
+plt.ylabel('Intrinsic reliability')
+plt.xlabel('Window width (ms)')
+
+plt.subplot(132)
+plt.title(r'\textbf{B} DRN 5HT', loc = 'left')
+plt.axvline(8, color = 'k', ls = '--', dashes = (10, 10), lw = 0.5)
+plt.semilogx(windows_arrs['5HT'], reliabilities['5HT'], '-', color = (0.9, 0.1, 0.1), lw = 0.8, alpha = 0.8)
+plt.ylim(0,1)
+plt.xlabel('Window width (ms)')
+
+plt.subplot(133)
+plt.title(r'\textbf{C} DRN SOM', loc = 'left')
+plt.axvline(8, color = 'k', ls = '--', dashes = (10, 10), lw = 0.5)
+plt.semilogx(windows_arrs['GABA'], reliabilities['GABA'], '-', color = (0.1, 0.7, 0.1), lw = 0.8, alpha = 0.8)
+plt.ylim(0,1)
+plt.xlabel('Window width (ms)')
+
+plt.tight_layout()
+
+if IMG_PATH is not None:
+    plt.savefig(IMG_PATH + 'intrinsic_reliability.png')
+
+plt.show()
