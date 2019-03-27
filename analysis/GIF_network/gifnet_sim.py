2df852b802367e861f177eb4fba15e3703381a81
Emerson Harkin <emerson.f.harkin@gmail.com>
2019-03-27 11:05:36 -0400

    More work in progress on GIFnet

--- analysis/GIF_network/gifnet_sim.py
+++ analysis/GIF_network/gifnet_sim.py
@@ -12,40 +12,176 @@ import pickle
 import multiprocessing as mp
 
 import numpy as np
+import pandas as pd
 
+import sys
+sys.path.append('./analysis/feedforward_gain_modulation')
+from FeedForwardDRN import SynapticKernel
 import src.GIF_network as gfn
+from src.Tools import generateOUprocess, timeToIntVec
 
 #%% PARSE COMMANDLINE ARGUMENTS
 
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    'model',
-    help = 'Path to GIFnet model used to run the simulation.'
-)
-parser.add_argument(
-    'destination_path',
-    help = 'Filepath for storing the results of the simulation.'
-)
-parser.add_argument(
-    '--input_tau', type = float,
-    help = 'Time constant of synaptic input alpha kernel.',
-    default = 15.
-)
-parser.add_argument(
-    '--processes', type = int,
-    help = 'Number of processes to spawn to accelerate simulations.',
-    default = 4.
-)
+if __name__ == '__main__':
 
-args = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'model',
+        help = 'Path to GIFnet model used to run the simulation.'
+    )
+    parser.add_argument(
+        'destination_path',
+        help = 'Filepath for storing the results of the simulation.'
+    )
+    parser.add_argument(
+        '--input_tau', type = float,
+        help = 'Time constant of synaptic input alpha kernel.',
+        default = 15.
+    )
+    parser.add_argument(
+        '--processes', type = int,
+        help = 'Number of processes to spawn to accelerate simulations.',
+        default = 4.
+    )
+    parser.add_argument(
+        '-v', '--verbose',
+        help = 'Increase output verbosity.',
+        action = 'store_true'
+    )
+
+    args = parser.parse_args()
 
 
-#%% LOAD MODEL
+#%% LOAD MODEL AND INITIALIZE VARS NEEDED TO RUN SIMULATION
 
-with open(args.model, 'rb') as f:
-    gifnet_mod = pickle.load(f)
-    f.close()
+if __name__ == '__main__':
 
-distal_in = SynapticKernel(
-    'alpha', tau = 15, ampli = 1, kernel_len = 500, dt = dt
-).centered_kernel
+    ### Load model.
+    with open(args.model, 'rb') as f:
+        gifnet_mod = pickle.load(f)
+        f.close()
+
+    ### Define distal input.
+    distal_in = SynapticKernel(
+        'alpha', tau = args.input_tau, ampli = 1, kernel_len = 500, dt = gifnet_mod.dt
+    ).centered_kernel
+
+    scales = np.linspace(0.010, 0.200, 15)
+
+    ### Initialize random components
+    OU_noise = {
+        'ser': np.array(
+            [generateOUprocess(len(distal_in) * gifnet_mod.dt, 3, 0, 0.020, gifnet_mod.dt, random_seed = int(i + 1)) for i in range(gifnet_mod.connectivity_matrix.shape[0])]
+        ),
+        'gaba': np.array(
+            [generateOUprocess(len(distal_in) * gifnet_mod.dt, 3, 0, 0.020, gifnet_mod.dt, random_seed = int((i + 1) / np.pi)) for i in range(gifnet_mod.connectivity_matrix.shape[1])]
+        )
+    }
+
+
+#%% DEFINE A MAIN FUNC TO RUN THE SIMULATION IN PARALLEL
+
+if __name__ == '__main__':
+
+    def main(scale):
+        """Main method for simulating GIFnet.
+        Intended to be run in parallel using mp.Pool.map.
+        """
+
+        no_ser_neurons = gifnet_mod.connectivity_matrix.shape[0]
+        no_gaba_neurons = gifnet_mod.connectivity_matrix.shape[1]
+
+        I = scale * distal_in
+
+        # Iterate over GABA neurons providing input to a single 5HT cell.
+        gaba_outmat = np.empty((no_gaba_neurons, len(distal_in)), dtype = np.float32)
+        for gaba_no in range(no_gaba_neurons):
+            t, V, eta, v_T, spks = gifnet_mod.gaba_mod.simulate(
+                I + OU_noise['gaba'][gaba_no, :], gifnet_mod.gaba_mod.El
+            )
+            gaba_outmat[gaba_no, :] = np.convolve(
+                timeToIntVec(spks, distal_in.shape[0] * gifnet_mod.dt, gifnet_mod.dt), gifnet_mod.gaba_kernel, 'same'
+            ).astype(np.float32)
+
+            # Save a sample trace.
+            if gaba_no == 0:
+                gaba_ex = {'t': t, 'V': V, 'eta': eta, 'v_T': v_T, 'spks': spks, 'I': I}
+
+            if args.verbose:
+                print '\r{} simulating GABA neurons for scale {:.4f} - {:.1f}%'.format(
+                    gifnet_mod.name, scale, 100 * (gaba_no + 1) / no_gaba_neurons
+                ),
+        if args.verbose: print '\n',
+
+        # Add propagation delay to GABA input.
+        gaba_outmat = np.roll(gaba_outmat, gifnet_mod.propagation_delay, axis = 1)
+        gaba_outmat[:, :gifnet_mod.propagation_delay] = 0
+
+        # Transform GABA output into 5HT input using connectivity matrix.
+        gaba_inmat = np.dot(gifnet_mod.connectivity_matrix, gaba_outmat)
+
+        # Allocate arrays for 5HT spiketrains.
+        ser_spkvecs = {
+            'ib': np.zeros_like(distal_in, dtype = np.int16),
+            'reg': np.zeros_like(distal_in, dtype = np.int16)
+        }
+        # Create a dict to hold 5HT example traces.
+        ser_examples = {}
+
+        for ser_no in range(no_ser_neurons):
+            I_tmp = I + OU_noise['ser'][ser_no, :]
+
+            # Try with and without feed-forward inhibition
+            for ib_status, ib_multiplier in zip(('ib', 'reg'), (1, 0)):
+                t, V, eta, v_T, spks = gifnet_mod.ser_mod.simulate(
+                    I_tmp + gaba_inmat[ser_no, :] * ib_multiplier,
+                    gifnet_mod.ser_mod.El
+                )
+                ser_spkvecs[ib_status] += timeToIntVec(spks, distal_in.shape[0] * gifnet_mod.dt, gifnet_mod.dt)
+
+                # Save a sample trace.
+                if ser_no == 0:
+                    ser_examples[ib_status] = {'t': t, 'V': V, 'eta': eta, 'v_T': v_T, 'spks': spks, 'I': I}
+
+            if args.verbose:
+                print '\r{} simulating 5HT neurons for scale {:.4f} - {:.1f}%'.format(
+                    gifnet_mod.name, scale, 100 * (ser_no + 1) / no_ser_neurons
+                ),
+        if args.verbose: print '\n'
+
+
+        ff_results_tmp = pd.DataFrame(columns = ['Scale', 'Inhibition', 'Input', 'ser spks', 'ser ex', 'gaba ex'])
+        for ib_status in ('ib', 'reg'):
+            row = {
+                'Scale': scale,
+                'Inhibition': ib_status,
+                'Input': I,
+                'ser spks': ser_spkvecs[ib_status],
+                'ser ex': ser_examples[ib_status],
+                'gaba ex': gaba_ex
+            }
+            ff_results_tmp = ff_results_tmp.append(row, ignore_index = True)
+
+        return ff_results_tmp
+
+
+#%% EXECUTE MAIN FUNC IN PARALLEL
+
+if __name__ == '__main__':
+
+    p = mp.Pool(int(args.processes))
+    results_tmp = p.map(main, scales)
+    p.close()
+
+    if args.verbose:
+        print 'Done simulation for {}! Saving output...'.format(gifnet_mod.name)
+
+    results = pd.concat(results_tmp, axis = 0, ignore_index = True)
+
+
+    with open(args.destination_path, 'wb') as f:
+        pickle.dump(results, f)
+        f.close()
+
+    if args.verbose:
+        print 'Finished {}!'.format(gifnet_mod.name)
--- makefiles/GIFnets.mk
+++ makefiles/GIFnets.mk
@@ -3,8 +3,18 @@ SIM_PATH=$(data/simulations/GIF_network/)
 .PHONY : all
 all : data/simulations/GIF_network/median_gifs.dat data/simulations/GIF_network/sergif_manual.dat data/simulations/GIF_network/sergif_noIA.dat
 
-data/models/GIF_network/%.mod : analysis/GIF_network/generate_gif_models.py
+data/models/GIF_network/median_gifs.mod : analysis/GIF_network/generate_gif_models.py
+	python -m analysis.GIF_network.generate_gif_models
+
+data/models/GIF_network/sergif_manual.mod : analysis/GIF_network/generate_gif_models.py
+	python -m analysis.GIF_network.generate_gif_models
+
+data/models/GIF_network/sergif_noIA.mod : analysis/GIF_network/generate_gif_models.py
 	python -m analysis.GIF_network.generate_gif_models
 
 data/simulations/GIF_network/%.dat : data/models/GIF_network/%.mod
-	@echo Floopabloop > $@
+	python -m analysis.GIF_network.gifnet_sim $< $@ -v
+
+.PHONY : clean
+clean :
+	rm data/models/GIF_network/*; rm data/simulations/GIF_network/*;
