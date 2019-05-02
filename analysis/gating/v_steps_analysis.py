Fix v_steps_analysis DF normalization

analysis/gating/v_steps_analysis.py
@@ -3,6 +3,7 @@
 from __future__ import division
 
 import pickle
+import os
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -94,129 +95,34 @@ def subtract_leak(cell, baseline_range, test_range, V_channel = 1, I_channel = 0
 
 #%% LOAD DATA
 
-FIGDATA_PATH = './figs/figdata/'
-GATING_PATH = './data/gating/'
-
-# Load V-steps files for sample pharma traces.
-baseline = Cell().read_ABF('./figs/figdata/18411010.abf')[0]
-TEA = Cell().read_ABF('./figs/figdata/18411013.abf')[0]
-TEA_4AP = Cell().read_ABF('./figs/figdata/18411015.abf')[0]
-
-# Load drug washin files
-TEA_washin = Cell().read_ABF([FIGDATA_PATH + '18411020.abf',
-                              FIGDATA_PATH + '18411012.abf',
-                              FIGDATA_PATH + '18412002.abf'])
-TEA_4AP_washin = Cell().read_ABF([FIGDATA_PATH + '18411022.abf',
-                                  FIGDATA_PATH + '18411014.abf'])
-XE_washin = Cell().read_ABF([GATING_PATH + '18619021.abf',
-                             GATING_PATH + '18614035.abf'])
+OUTPUT_PATH = os.path.join('data', 'processed', 'gating')
+GATING_PATH = os.path.join('data', 'gating')
 
 # Load gating data
-gating = Cell().read_ABF([GATING_PATH + '18411002.abf',
-                          GATING_PATH + '18411010.abf',
-                          GATING_PATH + '18411017.abf',
-                          GATING_PATH + '18411019.abf',
-                          GATING_PATH + 'c0_inact_18201021.abf',
-                          GATING_PATH + 'c1_inact_18201029.abf',
-                          GATING_PATH + 'c2_inact_18201034.abf',
-                          GATING_PATH + 'c3_inact_18201039.abf',
-                          GATING_PATH + 'c4_inact_18213011.abf',
-                          GATING_PATH + 'c5_inact_18213017.abf',
-                          GATING_PATH + 'c6_inact_18213020.abf',
-                          GATING_PATH + '18619018.abf',
-                          GATING_PATH + '18614032.abf'])
+gating = Cell().read_ABF([os.path.join(GATING_PATH, '18411002.abf'),
+                          os.path.join(GATING_PATH, '18411010.abf'),
+                          os.path.join(GATING_PATH, '18411017.abf'),
+                          os.path.join(GATING_PATH, '18411019.abf'),
+                          os.path.join(GATING_PATH, 'c0_inact_18201021.abf'),
+                          os.path.join(GATING_PATH, 'c1_inact_18201029.abf'),
+                          os.path.join(GATING_PATH, 'c2_inact_18201034.abf'),
+                          os.path.join(GATING_PATH, 'c3_inact_18201039.abf'),
+                          os.path.join(GATING_PATH, 'c4_inact_18213011.abf'),
+                          os.path.join(GATING_PATH, 'c5_inact_18213017.abf'),
+                          os.path.join(GATING_PATH, 'c6_inact_18213020.abf'),
+                          os.path.join(GATING_PATH, '18619018.abf'),
+                          os.path.join(GATING_PATH, '18614032.abf')])
 
-beautiful_gating_1 = Cell().read_ABF([GATING_PATH + '18619018.abf',
-                                      GATING_PATH + '18619019.abf',
-                                      GATING_PATH + '18619020.abf'])
+beautiful_gating_1 = Cell().read_ABF([os.path.join(GATING_PATH, '18619018.abf'),
+                                      os.path.join(GATING_PATH, '18619019.abf'),
+                                      os.path.join(GATING_PATH, '18619020.abf')])
 beautiful_gating_1 = Recording(np.array(beautiful_gating_1).mean(axis = 0))
 
-beautiful_gating_2 = Cell().read_ABF([GATING_PATH + '18614032.abf',
-                                      GATING_PATH + '18614033.abf',
-                                      GATING_PATH + '18614034.abf'])
+beautiful_gating_2 = Cell().read_ABF([os.path.join(GATING_PATH, '18614032.abf'),
+                                      os.path.join(GATING_PATH, '18614033.abf'),
+                                      os.path.join(GATING_PATH, '18614034.abf')])
 beautiful_gating_2 = Recording(np.array(beautiful_gating_2).mean(axis = 0))
 
-
-t_gating_bl = Cell().read_ABF([GATING_PATH + '18619038.abf',
-                               GATING_PATH + '18619039.abf'])
-t_gating_bl = Recording(np.array(t_gating_bl).mean(axis = 0))
-
-#t_gating_washon = Cell().read_ABF([GATING_PATH + '18619049.abf'])[0] # High and changing Ra
-
-#%% INSPECT RECORDINGS
-
-%matplotlib qt5
-beautiful_gating_1.plot(downsample = 1)
-
-beautiful_gating_2.plot(downsample = 1)
-
-t_gating_washon.plot(downsample = 1)
-
-t_gating_washon.fit_test_pulse((2240, 2248), (3250, 3300))
-
-# Look at XE991 (M-current blocker) washin
-XE_washin_tmp = XE_washin[1]
-XE_washin_tmp = subtract_baseline(XE_washin_tmp, slice(1000, 2000), 0)
-XE_washin_tmp = subtract_leak(XE_washin_tmp, slice(1000, 2000), slice(3000, 3400))
-
-plt.figure()
-plt.plot(XE_washin_tmp[0, 60000:61000, :].mean(axis = 0))
-plt.show()
-
-"""
-M-current blocker doesn't seem to affect leak- and baseline-subtracted current at -30mV.
-"""
-
-#%% PROCESS PHARMACOLOGY DATA
-
-# Example traces.
-sweep_to_use        = 10
-xrange              = slice(25000, 45000)
-xrange_baseline     = slice(24500, 25200)
-baseline_sweep      = baseline[0, xrange, sweep_to_use] - baseline[0, xrange_baseline, sweep_to_use].mean()
-TEA_sweep           = TEA[0, xrange, sweep_to_use] - TEA[0, xrange_baseline, sweep_to_use].mean()
-TEA_4AP_sweep       = TEA_4AP[0, xrange, sweep_to_use] - TEA_4AP[0, xrange_baseline, sweep_to_use].mean()
-cmd_sweep           = baseline[1, xrange, sweep_to_use] + TEA[1, xrange, sweep_to_use] / 2.
-
-# TEA washin.
-xrange_baseline     = slice(1000, 2000)
-xrange_testpulse    = slice(3000, 3500)
-xrange_ss           = slice(50000, 51000)
-TEA_washin_pdata    = np.empty((len(TEA_washin), TEA_washin[0].shape[1], 44))
-TEA_washin_pdata[:, :] = np.NAN
-
-for i, cell in enumerate(TEA_washin):
-
-    # Estimate Rm from test pulse and use to compute leak current.
-
-    TEA_washin_pdata[i, :, :cell.shape[2]] = subtract_leak(cell, xrange_baseline, xrange_testpulse)[0, :, :]
-    TEA_washin_pdata[i, :, :] -= np.nanmean(TEA_washin_pdata[i, xrange_baseline, :], axis = 0)
-    #TEA_washin_pdata[i, :, :] /= np.nanmean(TEA_washin_pdata[i, xrange_ss, :6])
-
-TEA_washin_pdata = np.nanmean(TEA_washin_pdata[:, xrange_ss, :], axis = 1).T
-
-# Remove data where there is no conductance left after subtracting leak.
-# (TEA can't have an effect if there's nothing to block.)
-TEA_washin_pdata = np.delete(
-TEA_washin_pdata,
-np.where(TEA_washin_pdata[:7, :].mean(axis = 0) <= 0)[0],
-axis = 1
-)
-
-# 4AP washin
-# Probably won't use...
-xrange_baseline         = slice(1000, 2000)
-xrange_testpulse        = slice(3000, 3500)
-xrange_ss               = slice(21770, 21800)
-TEA_4AP_washin_pdata    = np.empty((len(TEA_4AP_washin), TEA_4AP_washin[0].shape[1], 44))
-TEA_4AP_washin_pdata[:, :] = np.NAN
-
-for i, cell in enumerate(TEA_4AP_washin):
-    TEA_4AP_washin_pdata[i, :, :cell.shape[2]] = subtract_leak(cell, xrange_baseline, xrange_testpulse)[0, :, :]
-    TEA_4AP_washin_pdata[i, :, :] -= np.nanmean(TEA_4AP_washin_pdata[i, xrange_baseline, :], axis = 0)
-    #TEA_4AP_washin_pdata[i, :, :] /= np.nanmean(TEA_4AP_washin_pdata[i, xrange_ss, :6])
-
-TEA_4AP_washin_pdata = np.nanmean(TEA_4AP_washin_pdata[:, xrange_ss, :], axis = 1).T
 
 #%% PROCESS RAW GATING DATA
 
@@ -251,7 +157,7 @@ for i, cell in enumerate(gating):
 
 peakact_pdata[0, :, :]      /= peakact_pdata[1, :, :] - -101
 ss_pdata[0, :, :]           /= ss_pdata[1, :, :] - -101
-peakinact_pdata[0, :, :]    /= peakinact_pdata[1, :, :] - -101
+peakinact_pdata[0, :, :]    /= peakinact_pdata[1, -1, :] - -101 # Since driving force is same for all sweeps.
 
 # Average out small differences in cmd between cells due to Rs comp
 peakact_pdata[1, :, :]      = peakact_pdata[1, :, :].mean(axis = 1, keepdims = True)
@@ -263,13 +169,13 @@ peakinact_pdata[0, :, :] -= ss_pdata[0, :, :]
 
 # Pickle in case needed.
 
-with open(FIGDATA_PATH + 'peakact_pdata.pyc', 'wb') as f:
+with open(os.path.join(OUTPUT_PATH, 'peakact_pdata.dat'), 'wb') as f:
     pickle.dump(peakact_pdata, f)
 
-with open(FIGDATA_PATH + 'ss_pdata.pyc', 'wb') as f:
+with open(os.path.join(OUTPUT_PATH, 'ss_pdata.dat'), 'wb') as f:
     pickle.dump(ss_pdata, f)
 
-with open(FIGDATA_PATH + 'peakinact_pdata.pyc', 'wb') as f:
+with open(os.path.join(OUTPUT_PATH, 'peakinact_pdata.dat'), 'wb') as f:
     pickle.dump(peakinact_pdata, f)
 
 
@@ -358,6 +264,11 @@ peakact_params, peakact_fittedpts       = optimizer_wrapper(peakact_pdata, [12, 
 peakinact_params, peakinact_fittedpts   = optimizer_wrapper(peakinact_pdata, [12, -1, -60])
 ss_params, ss_fittedpts                 = optimizer_wrapper(ss_pdata, [12, 1, -25])
 
+for name, obj in {'peakact_fittedpts': peakact_fittedpts, 'peakinact_fittedpts': peakinact_fittedpts, 'ss_fittedpts': ss_fittedpts}.iteritems():
+    with open(os.path.join(OUTPUT_PATH, name + '.dat'), 'wb') as f:
+        pickle.dump(obj, f)
+        f.close()
+
 param_pickle_df = pd.DataFrame(
 {
 'm': peakact_params,
@@ -367,5 +278,5 @@ param_pickle_df = pd.DataFrame(
 index = ('A', 'k', 'V_half')
 )
 
-with open(FIGDATA_PATH + 'gating_params.pyc', 'wb') as f:
+with open(os.path.join(OUTPUT_PATH, 'gating_params.dat'), 'wb') as f:
     pickle.dump(param_pickle_df, f)