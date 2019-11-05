ad02c29523739bca25a0f6b0bfe117164b0d7894
Emerson Harkin <emerson.f.harkin@gmail.com>
2019-11-05 14:45:32 -0600

    Current step network simulations with tonic GABA
    Last round of simulations tested for an interaction of GABA/SOM input and IA by
    knocking out feedforward GABA input to 5HT cells. This approach did not
    distinguish between potential interactions between IA and tonic vs.
    stimulus-evoked GABA input.
    
    This round of simulations specifically addresses stimulus-evoked GABA input by
    allowing tonic GABA firing to proceed in the control condition.
    
    Visual inspection of the average 5HT population PSTHs suggests that IA and
    stimulus-evoked firing interact to decrease the amplitue and increase the
    breadth of the 5HT population response.

--- analysis/GIFnet_pipeline/run_pipeline.sh
+++ analysis/GIFnet_pipeline/run_pipeline.sh
@@ -13,9 +13,15 @@ PROCESSES=5
 python ./generate_models.py --sermods $SERMODS --gabamods $GABAMODS --prefix ../../data/models/GIF_network/GIFnet --opts gifnetmod_opts.json -r $REPEATS -v || exit 999
 
 # Generate input.
-python ../GIF_network/input_generators/current_step.py ../../data/simulations/GIF_network/input/square_step.dat \
+full_network_input=../../data/simulations/GIF_network/input/square_step_full.dat
+null_gaba_network_input=../../data/simulations/GIF_network/input/square_step_5HT_only.dat
+python ../GIF_network/input_generators/current_step.py $full_network_input \
 	--baseline-ser 0.01 --min-ser 0.010 --max-ser 0.060 \
 	--baseline-gaba 0. --min-gaba 0.010 --max-gaba 0.060 \
+	|| exit 999
+python ../GIF_network/input_generators/current_step.py $null_gaba_network_input \
+	--baseline-ser 0.01 --min-ser 0.010 --max-ser 0.060 \
+	--baseline-gaba 0. --min-gaba 0. --max-gaba 0. \
 	|| exit 999
 
 # Run simulations
@@ -25,10 +31,9 @@ for modtype in base noIA fixedIA; do
 		for i in $(seq 0 $[$REPEATS - 1]); do
 			python ./run_simulation.py $(if [ $i == 0 ]; then echo "-v"; fi) \
 				../../data/models/GIF_network/GIFnet_${i}_subsample_${modtype}.mod \
-				../../data/simulations/GIF_network/input/square_step.dat \
+				$(if [ $condition == GABA ]; then echo "$full_network_input"; elif [ $condition == noGABA ]; then echo "$null_gaba_network_input"; fi) \
 				../../data/simulations/GIF_network/subsample_${modtype}/${modtype}_${condition}_rep${i}.hdf5 \
-				--seed-background ${i} \
-				$(if [ $condition == noGABA ]; then echo "--no-gaba --no-feedforward"; fi) &
+				--seed-background ${i}
 			if [ $[($i + 1) % $PROCESSES] == 0 ]; then
 				wait
 			fi
