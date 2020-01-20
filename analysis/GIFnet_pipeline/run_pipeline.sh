807eabf4691d680487ea224f1900067d512b6ba4
Emerson Harkin <emerson.f.harkin@gmail.com>
2020-01-20 17:31:38 -0500

    Support for running all types of GABA controls
    Modify `run_pipeline.py` in `GIF_pipeline` to run GABA KO and GABA
    no-input controls.

--- analysis/GIFnet_pipeline/run_pipeline.sh
+++ analysis/GIFnet_pipeline/run_pipeline.sh
@@ -13,8 +13,8 @@ PROCESSES=5
 python ./generate_models.py --sermods $SERMODS --gabamods $GABAMODS --prefix ../../data/models/GIF_network/GIFnet --opts gifnetmod_opts.json -r $REPEATS -v || exit 999
 
 # Generate input.
-full_network_input=../../data/simulations/GIF_network/input/square_step_full.dat
-null_gaba_network_input=../../data/simulations/GIF_network/input/square_step_5HT_only.dat
+full_network_input=../../data/simulations/GIF_network/step_input/square_step_full.dat
+null_gaba_network_input=../../data/simulations/GIF_network/step_input/square_step_5HT_only.dat
 python ../GIF_network/input_generators/current_step.py $full_network_input \
 	--baseline-ser 0.01 --min-ser 0.010 --max-ser 0.060 \
 	--baseline-gaba 0. --min-gaba 0.010 --max-gaba 0.060 \
@@ -26,18 +26,51 @@ python ../GIF_network/input_generators/current_step.py $null_gaba_network_input 
 
 # Run simulations
 for modtype in base noIA fixedIA; do
-	for condition in GABA noGABA; do
-		echo "Starting $modtype $condition simulations"
-		for i in $(seq 0 $[$REPEATS - 1]); do
-			python ./run_simulation.py $(if [ $i == 0 ]; then echo "-v"; fi) \
-				../../data/models/GIF_network/GIFnet_${i}_subsample_${modtype}.mod \
-				$(if [ $condition == GABA ]; then echo "$full_network_input"; elif [ $condition == noGABA ]; then echo "$null_gaba_network_input"; fi) \
-				../../data/simulations/GIF_network/subsample_${modtype}/${modtype}_${condition}_rep${i}.hdf5 \
-				--seed-background ${i} &
-			if [ $[($i + 1) % $PROCESSES] == 0 ]; then
-				wait
-			fi
-		done
-	done
+
+    # GABA CONTROL
+    echo "Starting $modtype GABA control simulations."
+    for i in $(seq 0 $[$REPEATS - 1]); do
+        python ./run_simulation.py \
+            $(if [ $i == 0 ]; then echo "-v"; else echo "--num-ser-examples 0 --num-gaba-examples 0"; fi) \
+            ../../data/models/GIF_network/GIFnet_${i}_subsample_${modtype}.mod \
+            $full_network_input \
+            ../../data/simulations/GIF_network/step_input/DRN_$modtype/GABA_base/rep${i}.hdf5 \
+            --seed-background ${i} --sigma-background 0.002 &
+        if [ $[($i + 1) % $PROCESSES] == 0 ]; then
+            wait
+        fi
+    done
+    wait
+
+    # GABA KNOCKOUT
+    echo "Starting $modtype GABA KO simulations."
+    for i in $(seq 0 $[$REPEATS - 1]); do
+        python ./run_simulation.py \
+            $(if [ $i == 0 ]; then echo "-v"; else echo "--num-ser-examples 0 --num-gaba-examples 0"; fi) \
+            ../../data/models/GIF_network/GIFnet_${i}_subsample_${modtype}.mod \
+            $full_network_input \
+            ../../data/simulations/GIF_network/step_input/DRN_$modtype/GABA_KO/rep${i}.hdf5 \
+            --seed-background ${i} --sigma-background 0.002 \
+            --no-gaba &
+        if [ $[($i + 1) % $PROCESSES] == 0 ]; then
+            wait
+        fi
+    done
+    wait
+
+    echo "Starting $modtype GABA no input simulations."
+    for i in $(seq 0 $[$REPEATS - 1]); do
+        python ./run_simulation.py \
+            $(if [ $i == 0 ]; then echo "-v"; else echo "--num-ser-examples 0 --num-gaba-examples 0"; fi) \
+            ../../data/models/GIF_network/GIFnet_${i}_subsample_${modtype}.mod \
+            $null_gaba_network_input \
+            ../../data/simulations/GIF_network/step_input/DRN_$modtype/GABA_noinput/rep${i}.hdf5 \
+            --seed-background ${i} --sigma-background 0.002 &
+        if [ $[($i + 1) % $PROCESSES] == 0 ]; then
+            wait
+        fi
+    done
+    wait
+
 done
 
