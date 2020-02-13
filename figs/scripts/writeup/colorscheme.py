69f0b40bd27768b210bedf8998f464d73b1fbffa
Emerson Harkin <emerson.f.harkin@gmail.com>
2020-02-13 11:16:59 -0500

    Ser gating curves for publication and add colorscheme.py

---
+++ figs/scripts/writeup/colorscheme.py
@@ -0,0 +1,11 @@
+import seaborn as sns
+
+colors = {
+    'ser': sns.color_palette()[0],
+    'som': sns.color_palette()[1],
+    'pyr': 'gray',
+    'fit': 'black',
+    'm': sns.color_palette()[2],
+    'h': sns.color_palette()[3],
+    'n': sns.color_palette()[4]
+}
