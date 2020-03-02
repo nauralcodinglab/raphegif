1962d424b6b7cb0624e952894e0156547c6974b5
Emerson Harkin <emerson.f.harkin@gmail.com>
2020-03-02 13:50:20 -0500

    Script wave simulations

---
+++ analysis/GIFnet_pipeline/run_wave_simulations.sh
@@ -0,0 +1,60 @@
+# Set environment variables.
+export MKL_NUM_THREADS=2
+export NUMEXPR_NUM_THREADS=2
+export OMP_NUM_THREADS=2
+
+# Define constants.
+SERMODS=../../data/models/5HT/5HT_AugmentedGIFs.lmod
+GABAMODS=../../data/models/GABA/GABA_GIFs.lmod
+REPEATS=20
+PROCESSES=10
+
+wave_input=../../data/simulations/GIF_network/wave_input/wave_input.dat
+python ../GIF_network/input_generators/wave.py $wave_input -v || exit 999
+
+# DRN WITH GABA
+echo "Starting DRN GABA control simulations."
+for i in $(seq 0 $[$REPEATS - 1]); do
+    python ./run_simulation.py \
+        $(if [ $i == 0 ]; then echo "-v"; else echo "--num-ser-examples 0 --num-gaba-examples 0"; fi) \
+        ../../data/models/GIF_network/GIFnet_${i}_subsample_base.mod \
+        $wave_input \
+        ../../data/simulations/GIF_network/wave_input/DRN_subsample_base/GABA_base/rep${i}.hdf5 \
+        --seed-background ${i} --sigma-background 0.002 &
+    if [ $[($i + 1) % $PROCESSES] == 0 ]; then
+        wait
+    fi
+done
+wait
+
+# GABA KNOCKOUT
+echo "Starting DRN GABA KO simulations."
+for i in $(seq 0 $[$REPEATS - 1]); do
+    python ./run_simulation.py \
+        $(if [ $i == 0 ]; then echo "-v"; else echo "--num-ser-examples 0 --num-gaba-examples 0"; fi) \
+        ../../data/models/GIF_network/GIFnet_${i}_subsample_base.mod \
+        $wave_input \
+        ../../data/simulations/GIF_network/wave_input/DRN_subsample_base/GABA_KO/rep${i}.hdf5 \
+        --seed-background ${i} --sigma-background 0.002 \
+        --no-gaba &
+    if [ $[($i + 1) % $PROCESSES] == 0 ]; then
+        wait
+    fi
+done
+wait
+
+# mPFC
+echo "Starting mPFC simulations."
+for i in $(seq 0 $[$REPEATS - 1]); do
+    python ./run_simulation.py \
+        $(if [ $i == 0 ]; then echo "-v"; else echo "--num-ser-examples 0 --num-gaba-examples 0"; fi) \
+        ../../data/models/GIF_network/mPFC/GIFnet_${i}_subsample_base.mod \
+        $wave_input \
+        ../../data/simulations/GIF_network/wave_input/mPFC_base/GABA_KO/rep${i}.hdf5 \
+        --seed-background ${i} --sigma-background 0.002 \
+        --no-gaba &
+    if [ $[($i + 1) % $PROCESSES] == 0 ]; then
+        wait
+    fi
+done
+wait
