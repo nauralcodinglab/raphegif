691e8ab3db1525515b1ec667465f52f2b40a820c
Emerson Harkin <emerson.f.harkin@gmail.com>
2020-03-13 13:25:10 -0400

    Refresh single neuron model sample traces and benchmarks

--- figs/scripts/writeup/common.py
+++ figs/scripts/writeup/common.py
@@ -8,11 +8,23 @@ colors = {
     'm': sns.color_palette()[2],
     'h': sns.color_palette()[3],
     'n': sns.color_palette()[4],
-    '4AP': sns.color_palette()[5],
+    '4AP': (207./255., 2./255., 52./255.),  # Cherry red
     'input': 'gray',
+    'gif': (146./255., 10./255., 78./255.),  # Mulberry
+    'agif': (79./255., 145./255., 83./255.),  # Light forest
+    'igif': 'gray'
 }
 
-pvalue_thresholds = [[1e-4, '****'], [1e-3, '***'], [1e-2, '**'], [5e-2, '*'], [0.1, 'o'], [1., 'ns']]
+pvalue_thresholds = [
+    [1e-4, '****'],
+    [1e-3, '***'],
+    [1e-2, '**'],
+    [5e-2, '*'],
+    [0.1, 'o'],
+    [1.0, 'ns'],
+]
 
 sbarlw = 1.5  # Scale bar line width
 insetlw = 0.75  # Line width for dashed border used to mark insets.
+
+fliersize=2
