7afa6d7ec1139610420075c760263294efa986fd
Emerson Harkin <emerson.f.harkin@gmail.com>
2021-02-01 12:57:58 -0600

    Add accelerated method for computing PSTH
    Computing the PSTH of a spiketrain can take a long time because
    convolution is slow. This patch implements a new method for computing
    the PSTH that takes advantage of the sparsity of spiketrains. The new
    method is faster for spiketrains that are >65% sparse, and is
    selected automatically when it is expected to provide a speedup. For
    very sparse spiketrains, the new method is ~30X faster.
    
    Summary of changes
    ------------------
    
    - Implemented sparse convolution method as Spiketrain._sparse_PSTH
    - Redefined Spiketrain.PSTH to call either _sparse_PSTH or
      _dense_PSTH (old method) depending on spiketrain sparsity
    - Added unit tests to ensure _sparse_PSTH and _dense_PSTH produce
      identical output, up to numerical precision

--- grr/Spiketrain.py
+++ grr/Spiketrain.py
@@ -1,4 +1,5 @@
 import numpy as np
+import weave
 
 from .Trace import getRisingEdges
 
@@ -38,8 +39,27 @@ def getIndicesDuringSpikes(T, spikes_i, dt_after, initial_cutoff, dt):
 
 
 def PSTH(spktrain, window_width, no_neurons, dt=0.1):
+    """Obtain the population firing rate with a resolution of `window_width`.
+
+    Uses either np.convolve or an accelerated sparse convolution method
+    depending on the sparsity of spktrain.
+
     """
-    Obtain the population firing rate with a resolution of `window_width`.
+    # Based on rough benchmarks, the sparse method is faster when the
+    # spktrain is >65% zeros.
+    if (np.asarray(spktrain) == 0).sum() > (0.65 * len(spktrain)):
+        # Use sparse method if spktrain is more than 65% zeros.
+        return _sparse_PSTH(spktrain, window_width, no_neurons, dt=dt)
+    else:
+        # Use dense method if spktrain is <= 65% zeros.
+        return _dense_PSTH(spktrain, window_width, no_neurons, dt=dt)
+
+def _dense_PSTH(spktrain, window_width, no_neurons, dt=0.1):
+    """Obtain the population firing rate with a resolution of 
+    `window_width` using np.convolve.
+
+    Runtime is not affected by sparsity of spktrain.
+
     """
 
     window_width *= 1e-3
@@ -48,6 +68,55 @@ def PSTH(spktrain, window_width, no_neurons, dt=0.1):
     kernel = np.ones(int(window_width / dt)) / (window_width * no_neurons)
     psth = np.convolve(spktrain, kernel, 'same')
     return psth
+
+
+def _sparse_PSTH(spktrain, window_width, no_neurons, dt=0.1):
+    """Obtain the population firing rate with a resolution of `window_width`
+    using a sparse convolution method.
+
+    Runtime decreases with increasing sparsity of spktrain. Up to ~30X
+    faster than dense method for very sparse spktrains.
+
+    """
+    window_width *= 1e-3
+    dt *= 1e-3
+
+    p_kernel_length = int(window_width / dt)
+    p_kernel_weight = 1.0 / (window_width * no_neurons)
+    psth = np.zeros(len(spktrain) + p_kernel_length, dtype=np.float32)
+    spktrain = np.asarray(spktrain).astype(np.float32)
+    p_num_spktrain_timesteps = len(spktrain)
+
+    code = """
+    float kernel_weight = float(p_kernel_weight);
+    int kernel_length = int(p_kernel_length);
+    int num_spktrain_timesteps = int(p_num_spktrain_timesteps);
+
+    float num_spikes;  // Number of spikes in a single timestep.
+    for (int i=0; i<int(num_spktrain_timesteps); i++) {
+        num_spikes = spktrain[i];
+        if (num_spikes > 0.0) {
+
+            // Add kernel to PSTH.
+            float firing_rate_to_add = num_spikes * kernel_weight;
+            for (int j=0; j<kernel_length; j++) {
+                psth[i+j] += firing_rate_to_add;
+            }
+        }
+    }
+    """
+
+    vars = ['p_kernel_length', 'p_kernel_weight', 'psth', 'spktrain', 'p_num_spktrain_timesteps']
+
+    v = weave.inline(code, vars)
+
+    # Select part of PSTH to return that will match results
+    # of np.convolve(mode='same'). Very weird, but necessary 
+    # to get same results.
+    half_kernel_timesteps = p_kernel_length // 2
+    other_half_kernel_timesteps = p_kernel_length - half_kernel_timesteps
+    return psth[max(0, other_half_kernel_timesteps - 1):(len(psth) - half_kernel_timesteps - 1)]
+
 
 
 def getSpikeLatency(voltage, start_time, threshold=0., refractory_period=3., dt=0.1):
---
+++ grr/Spiketrain_test.py
@@ -0,0 +1,85 @@
+import unittest
+
+import numpy as np
+from numpy import testing as npt
+
+from grr import Spiketrain as spk
+
+class TestPSTH:
+    def test_even_kernel_odd_train(self):
+        """Even length kernel, odd length spktrain."""
+        spktrain = [0, 0, 0, 1, 0, 0, 2, 0, 1]
+        dt = 1000.  # 1000 ms = 1 s
+        kernel_width = 4000.  # 4000 ms = 4s
+        expected = [0, 0, 0.25, 0.25, 0.25, 0.75, 0.5, 0.75, 0.75]
+        npt.assert_array_equal(expected, self.psth_func(spktrain, kernel_width, 1, dt=dt))
+
+    def test_odd_kernel_odd_train(self):
+        """Odd length kernel, odd length spktrain."""
+        spktrain = [0, 0, 0, 1, 0, 0, 2, 0, 1]
+        dt = 1000.  # 1000 ms = 1 s
+        kernel_width = 3000.  # 3000 ms = 3s
+        expected = [0, 0, 0.33, 0.33, 0.33, 0.67, 0.67, 1.0, 0.33]
+        npt.assert_allclose(expected, self.psth_func(spktrain, kernel_width, 1, dt=dt), atol=0.01)
+
+    def test_even_kernel_even_train(self):
+        spktrain = [0, 0, 0, 1, 0, 0, 2, 0, 1, 0]
+        dt = 1000.  # 1000 ms = 1 s
+        kernel_width = 4000.  # 4000 ms = 4s
+        expected = [0, 0., 0.25, 0.25, 0.25, 0.75, 0.5, 0.75, 0.75, 0.25]
+        npt.assert_array_equal(expected, self.psth_func(spktrain, kernel_width, 1, dt=dt))
+
+    def test_odd_kernel_even_train(self):
+        """Odd length kernel, even length spktrain."""
+        spktrain = [0, 0, 0, 1, 0, 0, 2, 0, 1, 0]
+        dt = 1000.  # 1000 ms = 1 s
+        kernel_width = 3000.  # 3000 ms = 3s
+        expected = [0, 0, 0.33, 0.33, 0.33, 0.67, 0.67, 1.0, 0.33, 0.33]
+        npt.assert_allclose(expected, self.psth_func(spktrain, kernel_width, 1, dt=dt), atol=0.01)
+
+
+class SparsePSTH(TestPSTH, unittest.TestCase):
+    def setUp(self):
+        self.psth_func = spk._sparse_PSTH
+
+
+class DensePSTH(TestPSTH, unittest.TestCase):
+    def setUp(self):
+        self.psth_func = spk._dense_PSTH
+
+
+class ComparePSTHLongSpktrain(unittest.TestCase):
+    """Ensure PSTH methods always produce same results on a long spiketrain."""
+
+    def setUp(self):
+        self.spktrain = np.zeros(10000)
+        self.spktrain[200] = 2
+        self.spktrain[500] = 1
+        self.spktrain[550] = 1
+        self.spktrain[7000] = 1
+        self.spktrain[7103] = 1
+        self.spktrain[7600] = 3
+
+    def test_small_dt_short_kernel_one_neuron(self):
+        dense = spk._dense_PSTH(self.spktrain, 10.0, 1, 0.1)
+        sparse = spk._sparse_PSTH(self.spktrain, 10.0, 1, 0.1)
+        npt.assert_allclose(dense, sparse)
+
+    def test_large_dt_short_kernel_one_neuron(self):
+        dense = spk._dense_PSTH(self.spktrain, 10.0, 1, 10.)
+        sparse = spk._sparse_PSTH(self.spktrain, 10.0, 1, 10.)
+        npt.assert_allclose(dense, sparse)
+
+    def test_small_dt_long_kernel_one_neuron(self):
+        dense = spk._dense_PSTH(self.spktrain, 100.0, 1, 0.1)
+        sparse = spk._sparse_PSTH(self.spktrain, 100.0, 1, 0.1)
+        npt.assert_allclose(dense, sparse)
+
+    def test_small_dt_medium_kernel_few_neurons(self):
+        dense = spk._dense_PSTH(self.spktrain, 40.0, 13, 0.1)
+        sparse = spk._sparse_PSTH(self.spktrain, 40.0, 13, 0.1)
+        npt.assert_allclose(dense, sparse)
+
+
+if __name__ == '__main__':
+    unittest.main()
