68f141752d8b10d1238c2e776231928c14ecb5f2
Emerson Harkin <emerson@efharkin.com>
2022-04-27 15:39:55 -0400

    Save gating curves in more stable file formats
    Switch from saving I_A gating data generated by
    analysis/gating/v_steps_analysis.py in Python pickle format to HDF5 and
    CSV. This commit updates figs/scripts/ser_gating.ipynb to use the new
    files (it is the only downstream script/notebook to directly use the
    output of analysis/gating/v_steps_analysis.py).
    
    Also simplify reading in recordings for v-steps analysis.

--- analysis/gating/v_steps_analysis.py
+++ analysis/gating/v_steps_analysis.py
@@ -2,11 +2,13 @@
 
 from __future__ import division
 
-import pickle
 import os
+from textwrap import dedent
 
 import numpy as np
 import pandas as pd
+import h5py
+from ezephys.rectools import ABFLoader
 
 from grr.cell_class import Cell, subtract_baseline, subtract_leak
 from grr.CurveFit import fit_gating_curve
@@ -14,27 +16,27 @@ from grr.CurveFit import fit_gating_curve
 
 #%% LOAD DATA
 
-OUTPUT_PATH = os.path.join('data', 'processed', 'gating')
-GATING_PATH = os.path.join('data', 'raw', '5HT', 'gating')
+OUTPUT_PATH = os.path.join(os.getenv('DATA_PATH'), 'processed', '5HT')
+if not os.path.isdir(OUTPUT_PATH):
+    os.makedirs(OUTPUT_PATH, mode=755)
 
 # Load gating data
-gating = Cell().read_ABF(
+rec_index = pd.read_csv(
+    os.path.join(os.getenv('DATA_PATH'), 'raw', '5HT', 'gating', 'index.csv')
+)
+
+l = ABFLoader()
+ACCESS_RESISTANCE_CUTOFF = 30.0
+INCLUSION_QUERY = (
+    'access_resistance_megaohm <= @ACCESS_RESISTANCE_CUTOFF and include'
+)
+gating = l.load(
     [
-        os.path.join(GATING_PATH, '18411002.abf'),
-        os.path.join(GATING_PATH, '18411010.abf'),
-        os.path.join(GATING_PATH, '18411017.abf'),
-        os.path.join(GATING_PATH, '18411019.abf'),
-        os.path.join(GATING_PATH, 'c0_inact_18201021.abf'),
-        os.path.join(GATING_PATH, 'c1_inact_18201029.abf'),
-        os.path.join(GATING_PATH, 'c2_inact_18201034.abf'),
-        os.path.join(GATING_PATH, 'c3_inact_18201039.abf'),
-        os.path.join(GATING_PATH, 'c4_inact_18213011.abf'),
-        os.path.join(GATING_PATH, 'c5_inact_18213017.abf'),
-        os.path.join(GATING_PATH, 'c6_inact_18213020.abf'),
-        os.path.join(GATING_PATH, '18619018.abf'),
-        os.path.join(GATING_PATH, '18614032.abf'),
+        os.path.join(os.getenv('DATA_PATH'), 'raw', '5HT', 'gating', fname)
+        for fname in rec_index.query(INCLUSION_QUERY)['filename']
     ]
 )
+del l
 
 #%% PROCESS RAW GATING DATA
 
@@ -67,10 +69,11 @@ for i, cell in enumerate(gating):
     peakinact_pdata[0, :, i] = cell[0, xrange_peakinact, :].mean(axis=0)
     peakinact_pdata[1, :, i] = cell[1, xrange_peakact, :].mean(axis=0)
 
-peakact_pdata[0, :, :] /= peakact_pdata[1, :, :] - -101
-ss_pdata[0, :, :] /= ss_pdata[1, :, :] - -101
+E_K = 101.  # Reversal potential of potassium in mV.
+peakact_pdata[0, :, :] /= peakact_pdata[1, :, :] - E_K
+ss_pdata[0, :, :] /= ss_pdata[1, :, :] - E_K
 peakinact_pdata[0, :, :] /= (
-    peakinact_pdata[1, -1, :] - -101
+    peakinact_pdata[1, -1, :] - E_K
 )  # Since driving force is same for all sweeps.
 
 # Average out small differences in cmd between cells due to Rs comp
@@ -83,15 +86,34 @@ peakinact_pdata[0, :, :] -= ss_pdata[0, :, :]
 
 # Pickle in case needed.
 
-with open(os.path.join(OUTPUT_PATH, 'peakact_pdata.dat'), 'wb') as f:
-    pickle.dump(peakact_pdata, f)
+with h5py.File(
+    os.path.join(OUTPUT_PATH, 'gating_pdata_room_temp.h5'), 'w'
+) as f:
+    f.attrs['description'] = dedent(
+        """
+        Voltage-dependence of whole-cell currents in 5-HT neurons at room
+        temperature.
 
-with open(os.path.join(OUTPUT_PATH, 'ss_pdata.dat'), 'wb') as f:
-    pickle.dump(ss_pdata, f)
+        5-HT neurons express the transient voltage-dependent potassium current
+        I_A as well as another non-inactivating voltage-dependent potassium
+        current.
 
-with open(os.path.join(OUTPUT_PATH, 'peakinact_pdata.dat'), 'wb') as f:
-    pickle.dump(peakinact_pdata, f)
+        The arrays in this file contain measurements of the amount of whole
+        cell that can be used to construct voltage-dependence curves for the
+        activation and inactivation gates of I_A and the activation gate of
+        the non-inactivating current (`peakact`, `peakinact`, and `ss`
+        datasets, respectively). The dimensionality of each array is
+        `[channel, measurement, cell]`, where `channel == 0` is the current in
+        pA and `channel == 1` is the voltage at which the current was recorded
+        in mV. `plt.plot(d[1, ...] d[0, ...])` can be used to plot the gating
+        curves of all cells, where `d` is one of the arrays in this file.
 
+        """[1:]
+    )
+    f.create_dataset('peakact', data=peakact_pdata)
+    f.create_dataset('peakinact', data=peakinact_pdata)
+    f.create_dataset('ss', data=ss_pdata)
+    f.close()
 
 #%% FIG SIGMOID CURVES TO GATING DATA
 
@@ -103,19 +125,51 @@ peakinact_params, peakinact_fittedpts = fit_gating_curve(
 )
 ss_params, ss_fittedpts = fit_gating_curve(ss_pdata, [12, 1, -25])
 
-for name, obj in {
-    'peakact_fittedpts': peakact_fittedpts,
-    'peakinact_fittedpts': peakinact_fittedpts,
-    'ss_fittedpts': ss_fittedpts,
-}.iteritems():
-    with open(os.path.join(OUTPUT_PATH, name + '.dat'), 'wb') as f:
-        pickle.dump(obj, f)
-        f.close()
+with h5py.File(
+    os.path.join(OUTPUT_PATH, 'gating_fittedpts_room_temp.h5'), 'w'
+) as f:
+    f.attrs['description'] = dedent(
+        """
+        Voltage-dependence of whole-cell currents in 5-HT neurons at room
+        temperature.
 
-param_pickle_df = pd.DataFrame(
-    {'m': peakact_params, 'h': peakinact_params, 'n': ss_params},
-    index=('A', 'k', 'V_half'),
-)
+        5-HT neurons express the transient voltage-dependent potassium current
+        I_A as well as another non-inactivating voltage-dependent potassium
+        current.
 
-with open(os.path.join(OUTPUT_PATH, 'gating_params.dat'), 'wb') as f:
-    pickle.dump(param_pickle_df, f)
+        The arrays in this file contain sigmoid curves fitted to measurements
+        of current over voltage for several cells.
+
+        """[1:]
+    )
+    f.create_dataset('peakact', data=peakact_fittedpts)
+    f.create_dataset('peakinact', data=peakinact_fittedpts)
+    f.create_dataset('ss', data=ss_fittedpts)
+    f.close()
+
+with open(os.path.join(OUTPUT_PATH, 'gating_params.csv'), 'w') as f:
+    included_recs = rec_index.query(INCLUSION_QUERY)
+    f.write(
+        (
+            '# num_cells={}\n'
+            '# access_resistance_cutoff_megaohm={}\n'
+            '# access_resistance_mean_megaohm={:.2f}\n'
+            '# access_resistance_std_megaohm={:.2f}\n'
+            '# m="I_A activation gate"\n'
+            '# h="I_A inactivation gate"\n'
+            '# n="Non-inactivating current activation gate"\n'
+            '# A="Sigmoid scaling factor"\n'
+            '# k="Sigmoid slope"\n'
+            '# V_half="Sigmoid location (half-activation voltage in mV)"\n'
+        ).format(
+            included_recs.shape[0],
+            ACCESS_RESISTANCE_CUTOFF,
+            included_recs['access_resistance_megaohm'].mean(),
+            included_recs['access_resistance_megaohm'].std(),
+        )
+    )
+    pd.DataFrame(
+        {'m': peakact_params, 'h': peakinact_params, 'n': ss_params},
+        index=('A', 'k', 'V_half'),
+    ).to_csv(f)
+    f.close()
