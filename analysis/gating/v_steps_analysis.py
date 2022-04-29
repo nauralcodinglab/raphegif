2558e352d7a9278c09453b43a7a7bae78270906f
Emerson Harkin <emerson@efharkin.com>
2022-04-29 10:39:32 -0400

    Extract data processing from v_steps_analysis.py
    Move all logic for extracting current and voltage measurements from
    voltage step experiments into a new function extract_gating_data() in
    module grr.CurveFit. This will allow code reuse across analysis of
    experiments performed at room temperature and with a heated bath.

--- analysis/gating/v_steps_analysis.py
+++ analysis/gating/v_steps_analysis.py
@@ -5,13 +5,11 @@ from __future__ import division
 import os
 from textwrap import dedent
 
-import numpy as np
 import pandas as pd
 import h5py
 from ezephys.rectools import ABFLoader
 
-from grr.cell_class import Cell, subtract_baseline, subtract_leak
-from grr.CurveFit import fit_gating_curve
+from grr.CurveFit import extract_gating_data, fit_gating_curve
 
 
 #%% LOAD DATA
@@ -40,51 +38,15 @@ del l
 
 #%% PROCESS RAW GATING DATA
 
-# Define time intervals from which to grab data.
-xrange_baseline = slice(0, 2000)
-xrange_test = slice(3500, 4000)
-xrange_peakact = slice(26140, 26160)
-xrange_ss = slice(55000, 56000)
-xrange_peakinact = slice(56130, 56160)
-
-# Format will be [channel, sweep, cell]
-# Such that we can use plt.plot(pdata[0, :, :], pdata[1, :, :], '-') to plot I over V by cell.
-
-shape_pdata = (2, gating[0].shape[2], len(gating))
-
-peakact_pdata = np.empty(shape_pdata)
-ss_pdata = np.empty(shape_pdata)
-peakinact_pdata = np.empty(shape_pdata)
-
-for i, cell in enumerate(gating):
-
-    cell = subtract_baseline(cell, xrange_baseline, 0)
-    cell = subtract_leak(cell, xrange_baseline, xrange_test)
-
-    # Average time windows to get leak-subtracted IA and KSlow currents
-    peakact_pdata[:, :, i] = cell[:, xrange_peakact, :].mean(axis=1)
-    ss_pdata[:, :, i] = cell[:, xrange_ss, :].mean(axis=1)
-
-    # Get prepulse voltage for peakinact
-    peakinact_pdata[0, :, i] = cell[0, xrange_peakinact, :].mean(axis=0)
-    peakinact_pdata[1, :, i] = cell[1, xrange_peakact, :].mean(axis=0)
-
-E_K = 101.  # Reversal potential of potassium in mV.
-peakact_pdata[0, :, :] /= peakact_pdata[1, :, :] - E_K
-ss_pdata[0, :, :] /= ss_pdata[1, :, :] - E_K
-peakinact_pdata[0, :, :] /= (
-    peakinact_pdata[1, -1, :] - E_K
-)  # Since driving force is same for all sweeps.
-
-# Average out small differences in cmd between cells due to Rs comp
-peakact_pdata[1, :, :] = peakact_pdata[1, :, :].mean(axis=1, keepdims=True)
-ss_pdata[1, :, :] = ss_pdata[1, :, :].mean(axis=1, keepdims=True)
-peakinact_pdata[1, :, :] = peakinact_pdata[1, :, :].mean(axis=1, keepdims=True)
-
-# Remove contribution of KSlow to apparent inactivation peak.
-peakinact_pdata[0, :, :] -= ss_pdata[0, :, :]
-
-# Pickle in case needed.
+pdata = extract_gating_data(
+    gating,
+    (0, 2000),
+    (3500, 4000),
+    (26140, 26160),
+    (56130, 56160),
+    (55000, 56000),
+    'time_step',
+)
 
 with h5py.File(
     os.path.join(OUTPUT_PATH, 'gating_pdata_room_temp.h5'), 'w'
@@ -101,7 +63,7 @@ with h5py.File(
         The arrays in this file contain measurements of the amount of whole
         cell that can be used to construct voltage-dependence curves for the
         activation and inactivation gates of I_A and the activation gate of
-        the non-inactivating current (`peakact`, `peakinact`, and `ss`
+        the non-inactivating current (`activation_peak`, `inactivation_peak`, and `steady_state`
         datasets, respectively). The dimensionality of each array is
         `[channel, measurement, cell]`, where `channel == 0` is the current in
         pA and `channel == 1` is the voltage at which the current was recorded
@@ -110,20 +72,28 @@ with h5py.File(
 
         """[1:]
     )
-    f.create_dataset('peakact', data=peakact_pdata)
-    f.create_dataset('peakinact', data=peakinact_pdata)
-    f.create_dataset('ss', data=ss_pdata)
+
+    for k, v in pdata.iteritems():
+        f.create_dataset(k, data=v)
+
     f.close()
 
 #%% FIG SIGMOID CURVES TO GATING DATA
 
-peakact_params, peakact_fittedpts = fit_gating_curve(
-    peakact_pdata, [12, 1, -30]
-)
-peakinact_params, peakinact_fittedpts = fit_gating_curve(
-    peakinact_pdata, [12, -1, -60]
-)
-ss_params, ss_fittedpts = fit_gating_curve(ss_pdata, [12, 1, -25])
+params = {}
+fittedpts = {}
+initial_param_guess = {
+    'activation_peak': [12, 1, -30],
+    'inactivation_peak': [12, -1, -60],
+    'steady_state': [12, 1, -25],
+}
+
+for k, v in pdata.iteritems():
+    p, f = fit_gating_curve(v, initial_param_guess[k])
+    params[k] = p
+    fittedpts[k] = f
+
+del k, v, p, f, initial_param_guess
 
 with h5py.File(
     os.path.join(OUTPUT_PATH, 'gating_fittedpts_room_temp.h5'), 'w'
@@ -142,9 +112,10 @@ with h5py.File(
 
         """[1:]
     )
-    f.create_dataset('peakact', data=peakact_fittedpts)
-    f.create_dataset('peakinact', data=peakinact_fittedpts)
-    f.create_dataset('ss', data=ss_fittedpts)
+
+    for k, v in fittedpts.iteritems():
+        f.create_dataset(k, data=v)
+
     f.close()
 
 with open(os.path.join(OUTPUT_PATH, 'gating_params.csv'), 'w') as f:
@@ -169,7 +140,11 @@ with open(os.path.join(OUTPUT_PATH, 'gating_params.csv'), 'w') as f:
         )
     )
     pd.DataFrame(
-        {'m': peakact_params, 'h': peakinact_params, 'n': ss_params},
+        {
+            'm': params['activation_peak'],
+            'h': params['inactivation_peak'],
+            'n': params['steady_state'],
+        },
         index=('A', 'k', 'V_half'),
     ).to_csv(f)
     f.close()
--- grr/CurveFit.py
+++ grr/CurveFit.py
@@ -4,7 +4,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import optimize
 
-from .Tools import raiseExpectedGot
+from .cell_class import subtract_baseline, subtract_leak
+from .Tools import raiseExpectedGot, timeToIndex
 
 
 def exponential_curve(p, t):
@@ -13,10 +14,7 @@ def exponential_curve(p, t):
     I = (A + C) * exp (-t/tau) + C
     p = [A, C, tau]
     """
-    A = p[0]
-    C = p[1]
-    tau = p[2]
-
+    A, C, tau = p
     return (A + C) * np.exp(-t / tau) + C
 
 
@@ -55,12 +53,7 @@ def sigmoid_curve(p, V):
     p = [A, k, V0]
     y = A / ( 1 + exp(-k * (V - V0)) )
     """
-    if len(p) != 3:
-        raiseExpectedGot('3-tuple', 'argument `p`', p)
-    A = p[0]
-    k = p[1]
-    V0 = p[2]
-
+    A, k, V0 = p
     return A / (1 + np.exp(-k * (V - V0)))
 
 
@@ -121,6 +114,186 @@ def fit_gating_curve(pdata, p0, max_norm=True):
 
 def _max_normalize(x, axis=0):
     return x / x.max(axis=axis)
+
+
+def extract_gating_data(
+    recordings,
+    baseline_window,
+    test_window,
+    activation_peak_window,
+    inactivation_peak_window,
+    steady_state_window,
+    window_unit='ms',
+    current_channel=0,
+    voltage_channel=1,
+    potassium_equilibrium_potential=-101.0,
+):
+    """Process voltage step experiments for fitting gating curves.
+
+    Intended to extract information about the activation and inactivation
+    gates of I_A and the activation gate of a non-inactivating potassium
+    current in 5-HT cells.
+
+    Parameters
+    ----------
+    recordings
+        List of `Recording`s of voltage step experiments.
+    baseline_window: (float, float) or [(float, float)]
+        Time window for extracting baseline. Voltage during this window should
+        usually be -70mV.
+    test_window: (float, float) or [(float, float)]
+        Time window during test pulse to use for estimating membrane
+        resistance.
+    activation_peak_window: (float, float) or [(float, float)]
+        Time window for estimating the voltage-dependence of I_A's activation
+        gate based on peak current.
+    inactivation_peak_window: (float, float) or [(float, float)]
+        Time window for estimating the voltage-dependence of I_A's inactivation
+        gate based on peak current.
+    steady_state_window: (float, float) or [(float, float)]
+        Time window for estimating the voltage-dependence of the activation
+        gate of the non-inactivating potassium current found in 5-HT cells.
+    window_unit: 'ms' or 'time_step'
+        Whether `baseline_window`, `test_window`, etc are given in ms or time
+        steps.
+    current_channel, voltage_channel: int
+        `Recording` channels corresponding to clamping current and command
+        voltage, respectively.
+    potassium_equilibrium_potential: float
+        Reversal potential of potassium in gating experiments (mV).
+
+    Returns
+    -------
+    dict
+
+    """
+    if not all([r.ndim == 3 for r in recordings]):
+        raise ValueError('Expected all recordings to be 3D arrays.')
+    if not all([r.shape[2] == recordings[0].shape[2] for r in recordings]):
+        raise ValueError(
+            'Expected all recordings to have the same number of sweeps.'
+        )
+    if not np.allclose([r.dt for r in recordings], recordings[0].dt):
+        raise ValueError(
+            'Expected all recordings to have the same sampling rate.'
+        )
+    num_sweeps = recordings[0].shape[2]
+    num_recordings = len(recordings)
+    dt = recordings[0].dt
+    windows = {
+        'baseline': _broadcast_time_window_to_slices(
+            baseline_window, window_unit, dt, num_recordings
+        ),
+        'test': _broadcast_time_window_to_slices(
+            test_window, window_unit, dt, num_recordings
+        ),
+        'activation_peak': _broadcast_time_window_to_slices(
+            activation_peak_window, window_unit, dt, num_recordings
+        ),
+        'inactivation_peak': _broadcast_time_window_to_slices(
+            inactivation_peak_window, window_unit, dt, num_recordings
+        ),
+        'steady_state': _broadcast_time_window_to_slices(
+            steady_state_window, window_unit, dt, num_recordings
+        ),
+    }
+    gating_data = {
+        k: np.empty((2, num_sweeps, num_recordings))
+        for k in ['activation_peak', 'inactivation_peak', 'steady_state']
+    }
+
+    for i, r_raw in enumerate(recordings):
+        r = subtract_leak(
+            subtract_baseline(r_raw, windows['baseline'][i], current_channel),
+            windows['baseline'][i],
+            windows['test'][i],
+        )
+
+        # Average current in each time window.
+        for k in gating_data:
+            gating_data[k][current_channel, :, i] = r[
+                current_channel, windows[k][i], :
+            ].mean(axis=0)
+
+        # Average command voltage in appropriate time window.
+        gating_data['activation_peak'][voltage_channel, :, i] = r[
+            voltage_channel, windows['activation_peak'][i], :
+        ].mean(axis=0)
+        gating_data['steady_state'][voltage_channel, :, i] = r[
+            voltage_channel, windows['steady_state'][i], :
+        ].mean(axis=0)
+        # Note: height of inactivation peak depends on voltage during
+        # activation part of protocol.
+        gating_data['inactivation_peak'][voltage_channel, :, i] = r[
+            voltage_channel, windows['activation_peak'][i], :
+        ].mean(axis=0)
+
+    # Divide currents by driving force to convert to conductance.
+    gating_data['activation_peak'][current_channel, :, :] /= (
+        gating_data['activation_peak'][voltage_channel, :, :]
+        - potassium_equilibrium_potential
+    )
+    gating_data['steady_state'][current_channel, :, :] /= (
+        gating_data['steady_state'][voltage_channel, :, :]
+        - potassium_equilibrium_potential
+    )
+    gating_data['inactivation_peak'][current_channel, :, :] /= (
+        gating_data['inactivation_peak'][voltage_channel, -1, :]
+        - potassium_equilibrium_potential
+    )  # Since driving force is same for all sweeps.
+
+    # Average out small differences in cmd between cells due to Rs comp
+    for k in gating_data:
+        gating_data[k][voltage_channel, ...] = gating_data[k][
+            voltage_channel, ...
+        ].mean(axis=1, keepdims=True)
+
+    # Remove contribution of non-inactivating conductance to inactivation peak.
+    gating_data['inactivation_peak'][current_channel, ...] -= gating_data[
+        'steady_state'
+    ][current_channel, ...]
+
+    return gating_data
+
+
+def _broadcast_time_window_to_slices(
+    time_window, time_unit, dt, broadcast_length
+):
+    return [
+        _time_window_to_slice(t, time_unit, dt)
+        for t in _broadcast_tuple(time_window, broadcast_length)
+    ]
+
+
+def _time_window_to_slice(time_window, time_unit, dt):
+    if time_unit == 'ms':
+        result = slice(
+            timeToIndex(time_window[0], dt), timeToIndex(time_window[1], dt)
+        )
+    elif time_unit in {'timestep', 'time_step'}:
+        result = slice(time_window[0], time_window[1])
+    else:
+        raiseExpectedGot(
+            "'ms' or 'time_step'", "argument 'time_unit'", time_unit
+        )
+    return result
+
+
+def _broadcast_tuple(tuple_or_tuples, length):
+    try:
+        if not all(
+            [len(t) == len(tuple_or_tuples[0]) for t in tuple_or_tuples]
+        ):
+            raise ValueError('Not all tuples have same length')
+        if len(tuple_or_tuples) != length:
+            raise ValueError(
+                'Expected 1 or {} tuples, got {}'.format(
+                    length, len(tuple_or_tuples)
+                )
+            )
+        return tuple_or_tuples
+    except TypeError:
+        return [tuple_or_tuples for _ in range(length)]
 
 
 def plot_linear_fit(x, y, ax=None, **pltargs):
